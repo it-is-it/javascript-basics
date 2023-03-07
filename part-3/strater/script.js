f488928403731b99fcc760ad09129c71338cabb4
Ishwor <ishwortimalsina39@gmail.com>
2023-03-07 08:17:45 +0545

    first one

---
+++ part-3/strater/script.js
@@ -0,0 +1,52 @@
+/*
+const day = 'monday';
+
+// using switch case
+switch (day) {
+    case 'monday':
+        console.log('plan course structure');
+        console.log('go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('prepare theory vedios');
+        break;
+    case 'wednesday':
+    case 'thrusday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+    default:
+        console.log('not a valid day!');
+}
+
+// using if-else
+if (day === 'monday') {
+    console.log('plan course structure');
+    console.log('go to coding meetup');
+}
+else if (day === 'tuesday') {
+    console.log('prepare theory vedios');
+}
+else if (day === 'wednesday' || day === 'thrusday') {
+    console.log('Write code examples');
+}
+else if (day === 'friday') {
+    console.log('Record videos');
+}
+else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+}
+else {
+    console.log('not a valid day!');
+}
+*/
+
+const age =23;
+age>=18 ? console.log ('I like to drink wine!') : ('I like to drink water!');
+
