38d538de357a8b37f6618784a68e6c04291eb172
Ishwor <ishwortimalsina39@gmail.com>
2023-03-08 18:54:53 +0545

    files are added

--- part-1/starter/script.js
+++ part-1/starter/script.js
@@ -1,255 +1,314 @@
 /*
+
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
-console.log('Jonas');
+console.log('jonas');
 console.log(23);
 
-let firstName = "Dipannita";
+let firstName = 'jonas';
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
-let arif_chutti = "A + C"
-console.log(arif_chutti);
+// variable name conventions
+let years = 3;
+let jonas_matilda = 'jm';
+let $function = 27;
+let name = 'jonas';
 
-let gulzar$nowshin = 'G + N'
-console.log(gulzar$nowshin);
+let Person = 'jonas';
+let PI = 3.1415;
 
-let pet = 'Pintu'
-let footballer = ('Messi')
-let myage = '16'
+let myFirstJob = 'coder';
+let myCurrentjob = 'teacher';
 
-console.log(myage);
-console.log(pet);
-console.log(footballer);
-*/
+let job1 = 'programmer';
+let job2 = 'teacher';
 
-/* 
-function myFunc() {
-    let x = 10
-    if (x == 10) {
-        var myVar = 30;
-    }
-    console.log(myVar + 10);
-}
-myFunc();
+console.log(myFirstJob);
 
 
+// datatypes
+true; //boolean value
+let JavascriptIsfun = true;
+console.log(JavascriptIsfun);
 
-kisu na dile global(sob jaygay pabe)
+console.log(typeof true);
+console.log(typeof JavascriptIsfun);
+console.log(typeof 23);
+console.log(typeof 'jonas');
 
-const dile declare er somoy initialize korte hobe. pore ar value set kora jabe na
-
-let hocche bracket scope
-
-var hocche function scope.
-*/
-
-
-
-
-
-
-
-/*
-let javascriptisfun = true;
-console.log(javascriptisfun);
-
-// console.log(typeof true);
-console.log(typeof javascriptisfun);
-// console.log(typeof 23);
-// console.log(typeof 'Jonas');
-
-javascriptisfun = 'yes!';
-console.log(typeof javascriptisfun);
+JavascriptIsfun = 'YES!';
+console.log(typeof JavascriptIsfun);
 
 let year;
 console.log(year);
 console.log(typeof year);
 
-year = 1995;
+year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
 
 
-
 let age = 30;
 age = 31;
 
-const Birthyear = 1991;
+const birthyear = 2002;
+// birthyear = 2001;
 
-// const Birthyear;
+// const job;
 
-var job = 'programmer';
+var job = 'Programmer';
 job = 'teacher';
 
-Firstman = 'shuvo';
-console.log(Firstman);
-*/
+lastname = 'shelby';
+console.log(lastname);
 
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 1991;
+console.log(ageJonas, ageSarah);
 
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2**3 means 2 to the power of 3 =2*2*2
 
+const firstName = 'thomas';
+const lastname = 'shelby';
+console.log(firstName + ' ' + lastname);
 
-
-/*
-
-
-// Math operators
-
-
-const now = 2022;
-const ageShuvo = now - 2007;
-const agePrity = now - 2002;
-console.log(ageShuvo, agePrity);
-
-console.log(ageShuvo * 2, ageShuvo / 10, 2 ** 3 );
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
-
-const firstName = 'Shuvo';
-const lastName = 'prity';
-console.log(firstName + ' ' + lastName);
-
-
-
-
-
-// Assingment operators
-
-
+// Assignment operators
 let x = 10 + 5; //15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 =100
-x ++; // x= x + 1
-x --; // x= x - 1
-x --; // x= x - 1
+x += 10; //x=x+10 =25
+x *= 4; //x= x * 4 = 100
+x++;
+x--;
+x--;
 console.log(x);
 
+// Comparision operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isfullage = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
 
 
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 1991;
 
-// Comparison operators
-
-
-console.log(agePrity > ageShuvo); // >, < , >=, <=
-console.log(ageShuvo >= 18);
-
-const isFullAge = ageShuvo >= 18;
-
-console.log(now - 2002 > now - 2007);
-*/
-
-/*
-const now = 2022;
-const ageShuvo = now - 2007;
-const agePrity = now - 2002;
-
-console.log(now - 2002 > now - 2007);
+console.log(25 - 10 - 1);
 
 let x, y;
-x = y = 25 - 10 - 5; // x = y = 10, x = 10
+x = y = 25 - 10 - 5; //x=y=10, x=10
 console.log(x, y);
 
-const averageAge = (agePrity + ageShuvo) / 2
-console.log(agePrity, ageShuvo , averageAge);
-*/
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah);
 
 
+// coding challenges #1
+const markHeight = 1.69;
+const johnHeight = 1.95;
+const markWeight = 78;
+const johnWeight = 92;
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / (johnHeight * johnHeight);
+const markHeigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHeigherBMI);
 
 
-/*
-// const massMark = 78;
-// const heighMark = 1.69;
-// const massJohn = 92;
-// const heightJohn = 1.95;
-
-const massMark = 95;
-const heighMark = 1.88;
-const massJohn = 85;
-const heightJohn = 1.76;
-
-const BMImark = massMark / heighMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-const markHigherBMI = BMImark > BMIJohn;
-console.log(BMImark, BMIJohn, markHigherBMI);
-*/
+const markHeight = 1.88;
+const johnHeight = 1.76;
+const markWeight = 95;
+const johnWeight = 85;
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / (johnHeight * johnHeight);
+const markHeigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHeigherBMI);
 
 
+const firstName = 'Thomas';
+const job = 'Businessman';
+const birthyear = 1920;
+const year = 1980;
 
-/*
-const firstName = 'Jonas';
-const job = 'teacher';
-const Birthyear = 1991;
-const year = 2037;
+const thomas =
+  "I'm " + firstName + ',a ' + (year - birthyear) + ' years old ' + job + '!';
+console.log(thomas);
 
-const jonas = "I'm" + firstName + ", a" + (year - Birthyear) + 'years old' + job + '!';
-console.log(jonas);
+const thomasNew = `I'm ${firstName}, a ${year - birthyear} years old ${job}!`;
+console.log(thomasNew);
 
-const jonasNew = `I'm ${firstName}, a ${year - Birthyear} years old ${job}!`;
-console.log(jonasNew);
+console.log(`just a regular string ......`);
 
-console.log(`Just a regular string....`);
+console.log(
+  //old methods of creating multiple new lines
+  'string with \n\
+multiple \n\
+lines'
+);
 
-
-console.log(`String
-multiple
+// new methods of writing multiple lines
+console.log(`string 
+with multiple
 lines`);
-*/
 
-
-
-/*
 const age = 15;
 
-if(age >= 18) {
-    console.log('Sarah can start driving license');
+if (age >= 18) {
+  console.log('Maria can start driving lisense 🚗');
 } else {
-    const yearleft = 18 - age;
-    console.log(`Sarah is too young. Wait another ${yearleft} years :)`)
+  const yearsleft = 18 - age;
+  console.log(`Maria can't drive the car until ${yearsleft} years old :D `);
 }
 
-const Birthyear = 2002;
+const birthyear = 2012;
 
 let century;
-if(Birthyear <= 2000) {
-    century = 20;
+if (birthyear <= 2000) {
+  century = 20;
 } else {
-    century = 21;
+  century = 21;
 }
 console.log(century);
-*/
 
-/*
-const massMark = 78;
-const heighMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
 
-// const massMark = 95;
-// const heighMark = 1.88;
-// const massJohn = 85;
-// const heightJohn = 1.76;
+// 2nd coding challenges
 
-const BMImark = massMark / heighMark ** 2;
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log(BMImark, BMIJohn);
-
-if(BMImark > BMIJohn) {
-    console.log(`Mark's BMI (${BMImark}) is higher than John's (${BMIJohn})!`)
+const markHeight = 1.88;
+const johnHeight = 1.76;
+const markWeight = 95;
+const johnWeight = 85;
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / (johnHeight * johnHeight);
+console.log(markBMI, johnBMI);
+if (markBMI > johnBMI) {
+  console.log(`mark's bmi (${markBMI}) is higher than john's. (${johnBMI})`);
 } else {
-    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMImark}) !`)
+  console.log(`john's bmi (${johnBMI}) is higher than mark's.(${markBMI})`);
 }
+
+
+
+// type conversion
+const inputyear = '1991';
+console.log(Number(inputyear), inputyear);
+console.log(Number(inputyear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+// console.log(string(23), 23);   not working in the new browser
+
+// type coercion
+console.log('I am ' + 23 + ' years old.');
+console.log('I am ' + '23' + ' years old.');
+console.log('23' - '10' - 3);
+console.log('23' + '10' + 3);
+console.log('23' * '2');
+console.log('23' / '2');
+
+// mindblowing convesion
+let n = '1' + 1; //n=11 because of adding a string
+n = n - 1; //here the value n=11 so 11-1 becomes 10
+console.log(n);
+
+
+
+// 5 falsy values : 0,'',undefined, null ,NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+
+const money = 100;
+if (money) {
+  console.log("don't spend it all :D");
+} else {
+  console.log('You should get a job!');
+}
+
+let height = 0;
+if (height) {
+  console.log('Yay Height is defined');
+} else {
+  console.log('Height is Undefined');
+}
+
+
+// equality operators
+
+console.log(18 === 18);
+console.log(19 === 18);
+console.log(18 == 18);
+console.log('18' === 18);
+
+const age = '18';
+if (age === 18) console.log('You just become an adult!(strict)');
+if (age == 18) console.log('You just become an adult!(loose)');
+const favriote = Number(prompt("What's your favroite number?"));
+console.log(favriote);
+console.log(typeof favriote);
+
+if (favriote === 23) {
+  //22 ===23 -> false
+  console.log('Cool! 23 is an amazing number!');
+} else if (favriote === 7) {
+  console.log('7 is also a cool number');
+} else if (favriote === 9) {
+  console.log('9 is also a cool number');
+} else {
+  console.log('Number is not 23 or 7 or 9');
+}
+
+if (favriote != 23) console.log('Why not 23?');
 */
 
+// boolean logic
+const hasDriverLisense = true; //A
+const hasGoodVision = true; //B
 
+console.log(hasDriverLisense && hasGoodVision);
+console.log(hasDriverLisense || hasGoodVision);
+console.log(!hasDriverLisense);
 
-const inputYear = '1991';
-console.log(Number(inputYear)); 
-console.log(Number(inputYear) + 18);
+if (hasDriverLisense && hasGoodVision) {
+  console.log('Sarah is able to drive');
+} else {
+  console.log('Someelse should drive the car');
+}
 
-// console.log(Number('Shuvo'));
-// console.log(typeof NaN);
+const isTried = false; //C
+console.log(hasDriverLisense && hasGoodVision && isTried);
 
+if (hasDriverLisense && hasGoodVision && !isTried) {
+  console.log('Sarah is able to drive');
+} else {
+  console.log('Someelse should drive the car');
+}
 
-console.log(String(23));
+//coding challenges
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+const scoreDolphins = (97 + 112 + 101) / 3;
+const scoreKoalas = (109 + 95 + 123) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log('Dolphins wins the trophy🫠');
+} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+  console.log('koalas wins the trophy😉');
+} else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log('Both team wins the trophy😎');
+} else {
+  console.log('No team wins the trophy😒');
+}
