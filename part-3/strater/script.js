b9135dfa9c974558baca0d2061ebc2e9e4bfb9c9
Ishwor <ishwortimalsina39@gmail.com>
2023-03-09 17:05:30 +0545

    new files are added

--- part-3/strater/script.js
+++ part-3/strater/script.js
@@ -3,4 +3,51 @@
 const x = '23';
 if (x===23) console.log(23);
 
-const calcAge = birthyear =1980-birthyear;
+const calcAge = (birthyear) => 1980 - birthyear;
+console.log(calcAge(1920));
+
+//problem solving using search engine
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) {
+      max = curTemp;
+    }
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = calcTempAmplitude(temperatures);
+console.log(amplitude);
+
+// new problem
+const calcTempAmp = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) {
+      max = curTemp;
+    }
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amp = calcTempAmp([3, 5, 1], [9, 0, 5]);
+console.log(amp);
