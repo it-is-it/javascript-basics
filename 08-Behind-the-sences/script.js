e889cd6daaefafea5499b40275a83cb733daafd3
Ishwor <ishwortimalsina39@gmail.com>
2023-06-08 22:20:12 +0545

    new commit

--- 08-Behind-the-sences/script.js
+++ 08-Behind-the-sences/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'walter    ';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+      function add(a, b) {
+        return a + b;
+      }
+    }
+
+    console.log(millenial);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Thomas';
+calcAge(1991);
