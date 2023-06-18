99a795609e6e41560d829380a5c01ccf9fe76504
Ishwor <ishwortimalsina39@gmail.com>
2023-06-18 21:21:48 +0545

    new commit

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -44,8 +44,80 @@ const restaurant = {
   orderPasta: function (ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1} ${ing2} ${ing3}`);
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
+// use any data type,return any data type, short-cuircuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined|| 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests =23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2= restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('----AND------');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && "h=jonas");
+
+
+if (restaurant.orderPizza) {
+  restaura.orderPizza('mushrooms' , 'spinach')
+}
+restaurant.orderPizza && restaurant.orderPizza ('mushrooms', 'S')
+
+/*
+// rest pattern and parameters
+// 1) Destructing
+
+// Spread, because on right side of =
+const arr = [1, 2, ...[3, 4]];
+
+// Rest because on Left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, risotto, otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2,3);
+add(5,3,7,2);
+add(8,2,5,3,2,1,4);
+
+const x = [23,5 ,7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+
+
+// spread operators
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -72,17 +144,27 @@ console.log(letters);
 console.log(...str);
 // console.log(`${...str} schmendantam`);
 
+// Real-world examples
 const ingredients = [
-  prompt("Let's make pasta! Ingredent 1?"),
-  prompt('Ingriedent 2?'),
-  prompt('Ingredent 3?'),
-]
+  // prompt("Let's make pasta! Ingredent 1?"),
+  // prompt('Ingriedent 2?'),
+  // prompt('Ingredent 3?'),
+];
 console.log(ingredients);
 
-restaurant.orderPasta(ingredients)[0],ingredients[1],ingredients[2]);
-restaurant.orderPasta(...ingredients)
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
 
-/*
+// Objects
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+
 // Destructuring objects
 restaurant.orderDelivery({
   time: '22:30',
