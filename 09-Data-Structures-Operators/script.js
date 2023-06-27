86948d27cdec86dadfab6236f1d15bfc072a1cc1
Ishwor <ishwortimalsina39@gmail.com>
2023-06-27 16:41:46 +0545

    new commit

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -56,19 +56,25 @@ if (restaurant.openingHours && restaurant.openingHours.mon)
 console.log(restaurant.openingHours.mon?.open);
 console.log(restaurant.openingHours?.mon?.open);
 
-
 const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-for (const day of days){
+for (const day of days) {
   console.log(day);
   const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log( `On ${day}, we open at ${open}`);
+  console.log(`On ${day}, we open at ${open}`);
 }
 
-
 // methods
-console.log(restaurant.order?.(0,1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0,1) ?? 'Method does not exist');
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
+// Arrays
+// const users = [{ name: 'Thomas', email: 'tommyshelby@gamil.com' }];
+const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty');
 
 /*
   const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
