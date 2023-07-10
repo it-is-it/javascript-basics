2e2e2a32d818c2137ec946e55171cb44ae0e0f09
Ishwor <ishwortimalsina39@gmail.com>
2023-07-10 22:07:58 +0545

    new commit

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -50,61 +50,118 @@ const restaurant = {
   },
 };
 
-if (restaurant.openingHours && restaurant.openingHours.mon)
-  console.log(restaurant.openingHours.mon.open);
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'correct'],
+  [false, 'Try again'],
+]);
+console.log(question);
 
-console.log(restaurant.openingHours.mon?.open);
-console.log(restaurant.openingHours?.mon?.open);
+// convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
 
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-for (const day of days) {
-  console.log(day);
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`On ${day}, we open at ${open}`);
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
 }
+const answer = Number(prompt('Your answer'));
+console.log(answer);
 
-// methods
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+console.log(question.get(question.get('correct') === answer));
 
-// Arrays
-// const users = [{ name: 'Thomas', email: 'tommyshelby@gamil.com' }];
-const users = [];
-
-console.log(users[0]?.name ?? 'User array empty');
-
-if (users.length > 0) console.log(users[0].name);
-else console.log('user array empty');
-else console.log('user array empty');
-
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
 /*
-  const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-  for (const item of menu) console.log(item);
-  for (const [i, el] of menu.entries()) {
-    console.log(`${i[0] + 1}: ${el}`);
-  }
 
-// console.log([...menu.entries()]);
+// Maps fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
 
-///////////////////////////////////////
-// Coding Challenge #1
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
 
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
 
-We're building a football betting app (soccer for my American friends 😅)!
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
 
-1. Create one player array for each team (variables 'players1' and 'players2')
-2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
-3. Create an array 'allPlayers' containing all players of both teams (22 players)
-4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
-5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
-6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
-7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
 
-TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+console.log(rest.get(arr));
+
+ 
+// Sets
+const ordersSet = new Set (['pasta','pizza','pizza','Risotto','pasta','pizza',]);
+console.log(ordersSet);
+
+console.log(new Set('Thomas')); 
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for(const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['waiter','chef','waiter','Manager','chef','waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(new Set(['waiter','chef','waiter','Manager','chef','waiter']).size);
+console.log(new Set('Thomas').size);
+
+//////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
 
 GOOD LUCK 😀
+*/
 
 const game = {
   team1: 'Bayern Munich',
@@ -147,6 +204,111 @@ const game = {
   },
 };
 
+/*
+
+// 1.
+for(const [i,player] of game.scored.entries()){
+  console.log(`goal ${i+1}: ${player}`);
+}
+
+// 2.
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds){
+  average += odd;
+}
+average/=odds.length;
+console.log(average);
+
+// 3.
+for(const [team, odd] of Object.entries(game.odds)){
+  const teamStr = team ==='x'? 'draw' : `victory ${game[team]}`
+  console.log(`Odd of${teamStr} ${odd}`);
+}
+
+
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr =`We are open on ${properties.length} days:`;
+for(const day of properties){
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+// Property Values
+const values = Object.values(openingHours)
+console.log(values);  
+
+
+//Entries object 
+const entries = Object.entries(openingHours);
+// console.log (entries);
+
+
+// [key, value]
+for(const [day, {open , close}] of entries){
+  console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+// const users = [{ name: 'Thomas', email: 'tommyshelby@gamil.com' }];
+const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users.length > 0) console.log(users[0].name);
+else console.log('user array empty');
+
+  const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+  for (const item of menu) console.log(item);
+  for (const [i, el] of menu.entries()) {
+    console.log(`${i[0] + 1}: ${el}`);
+  }
+
+// console.log([...menu.entries()]);
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+/*
 // 1
 const [players1, players2] = game.players;
 console.log(players1, players2);
