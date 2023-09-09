a0cb195ea97e24eeb7b5aa55fb6c6e7226c805f6
Ishwor <ishwortimalsina39@gmail.com>
2023-09-09 13:00:11 +0545

    new commit

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+//oop in javascript
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //never do this
+  //this.calcAge = function(){
+  //     console.log(2037-this.birthYear);
+  //   }
+};
+
+const thomas = new Person('Thomas', 1991);
+console.log(thomas);
+
+//1. New () is created
+// 2. function is called, this=()
+// 3. () linked to prototype
+// 4. funciton automatically return {}
+
+const monika = new Person('Monika', 1835);
+const john = new Person('John', 1953);
+console.log(monika, john);
+
+console.log(thomas instanceof Person);
+
+
+//Prototype
+console.log(Person.prototype)
+
+Person.prototype.calcAge = function(){
+        console.log(2037-this.birthYear);
+      }
+
+thomas.calcAge();
+monika.calcAge();
+
+
