1c38f10ea6da1321646a85cda2a0453dd6608aca
Ishwor <ishwortimalsina39@gmail.com>
2023-09-11 08:54:21 +0545

    new commit

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,123 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+/*
+
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `
+  <article class="country">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages.deu}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('france');
+getCountryData('germany');
+getCountryData('spain');
+
+*/
+
+/*
+
+const renderCountry = function (data) {
+  const html = `
+      <article class="country">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages.deu}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies.EUR.name
+            }</p>
+        </div>
+      </article>
+      `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // ajax call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // render country
+    renderCountry(data);
+
+    //get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // ajax call country 1
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2);
+    });
+  });
+};
+
+// getCountryAndNeighbour('france');
+getCountryAndNeighbour('germany');
+// getCountryAndNeighbour('spain');
+
+
+*/
+
+/*
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+        console.log('3 second passed');
+        setTimeout(() => {
+            console.log('4 second passed');
+          }, 1000);
+      }, 1000);
+  }, 1000);
+}, 1000);
+*/
+
+
+const getCountryData = function(country){
+    fetch(`https://restcountries.com/v3.1/name/${country}`).then(function (response){
+        console.log(response);
+       return response.json()
+    })
+}
+getCountryData('germany')
