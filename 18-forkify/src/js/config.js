4181abcd2c5f0fd83147c0cff827f8fa01169fab
Ishwor <ishwortimalsina39@gmail.com>
2023-09-15 13:47:01 +0545

    new commit

---
+++ 18-forkify/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8ccb33ef-3f4d-47a5-8fab-9998c6ad64ef';
+export const MODAL_CLOSE_SEC = 2.5;
