645aa0ae84bb4717ad469d1c230addc92d96f874
Ishwor <ishwortimalsina39@gmail.com>
2023-09-15 09:06:45 +0545

    new commit

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,15 +1,15 @@
 import * as model from './model.js';
+// import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+// import bookmarksView from './views/bookmarksView.js';
+// import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
-
-if (module.hot) {
-  module.hot.accept();
-}
 
 const controlRecipes = async function () {
   try {
@@ -18,13 +18,14 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
-    // 1 Loading reciepe
+    // 2) Loading recipe
     await model.loadRecipe(id);
 
-    // 2 rendering reciepe
+    // 3) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 
@@ -32,22 +33,44 @@ const controlSearchResults = async function () {
   try {
     resultsView.renderSpinner();
 
-    // 1 get search query
+    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
-    // 2 load search results
+    // 2) Load search results
     await model.loadSearchResults(query);
 
-    // 3 render results
-    resultsView.render(model.state.search.results);
+    // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
 
+const controlPagination = function (goToPage) {
+  // 1) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
